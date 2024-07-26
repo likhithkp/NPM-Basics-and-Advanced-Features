7d0f386be41cc0f773b8d0c693aba15cf78e8255
likhithkp <likhithkpdev@gmail.com>
2024-07-26 07:00:45 +0530

    Push

---
+++ npm.js
@@ -0,0 +1,28 @@
+//NPM understanding
+// npm (Node Package Manager) is a package manager for JavaScript and the world's largest software registry. It allows     developers to discover, share, and use code from other developers. npm is also used to manage project dependencies, ensuring that the right versions of libraries and tools are installed.
+
+// Key Features
+// ------------------
+// Package Management: Easily install, update, and uninstall packages.
+// Version Control: Ensure consistent dependencies with package versioning.
+// Scripts: Automate common tasks like testing, building, and deploying with npm scripts.
+// Registry: Access over 1 million packages published to the npm registry.
+
+// Prebuilt codes or code blocks that comes node is called Module.
+// Codes installed from NPM is called package.
+
+// Write npm i/install [package name] to install package, you can cross check it in package.json. Ex: npm i cors
+// npm uninstall [package name] to uninstall. Ex: npm uninstall cors
+// npm i [package name]@[version]. Ex: npm i accessibility@3.0.9
+
+// Node modules
+// ------------
+// Node modules contains all the installed packages with all the dependencies.
+
+// Dev dependencies
+// ----------------
+// Packages used only during development process. These dependencies will be neglected or skipped during deployment.
+
+// Scripts
+// -------
+// Commands prewritten or written in package.json to make meaningful commands.
